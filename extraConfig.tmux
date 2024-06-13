f90e8f153ff59f7b5e4ccbbc42e4b50212f12d9e
Boney Patel <boneypatel37@yahoo.com>
2024-06-13 18:31:41 -0400

    Home manager config and tmux config (WIP)

---
+++ extraConfig.tmux
@@ -0,0 +1,60 @@
+# Bind C-w + r to reload tmux config
+unbind r
+bind r source-file ~/.tmux.conf
+
+# Rebind <leader> to C-w
+set -g prefix C-w
+
+# Allow mouse to resize windows
+set -g mouse on
+
+# Copying from tmux
+bind -T copy-mode-vi v send -X begin-selection
+bind -T copy-mode-vi y send-keys -X copy-pipe-and-cancel "pbcopy"
+bind P paste-buffer
+bind -T copy-mode-vi MouseDragEnd1Pane send-keys -X copy-pipe-and-cancel "pbcopy"
+
+# Pane navigation bindings
+bind-key h select-pane -L
+bind-key j select-pane -D
+bind-key k select-pane -U
+bind-key l select-pane -R
+
+# Bind kill pane to not prompt confirmation
+bind-key x kill-pane
+
+# Rebind splitting panes
+bind n split-window -h -c "#{pane_current_path}"
+bind f split-window -v -c "#{pane_current_path}"
+
+# List of plugins
+set -g @plugin 'tmux-plugins/tpm'
+set -g @plugin 'tmux-plugins/tmux-sensible'
+
+# Vim tmux navigator
+set -g @plugin 'christoomey/vim-tmux-navigator'
+
+# Catppuccin for tmux
+set -g @plugin 'catppuccin/tmux'
+
+# Configs for Catppuccin tmux
+set -g @catppuccin_window_right_separator "█ "
+set -g @catppuccin_window_number_position "right"
+set -g @catppuccin_window_middle_separator " | "
+
+set -g @catppuccin_window_default_fill "none"
+
+set -g @catppuccin_window_current_fill "all"
+
+set -g @catppuccin_status_modules_right "application session"
+set -g @catppuccin_status_left_separator "█"
+set -g @catppuccin_status_right_separator "█"
+
+# Other examples:
+# set -g @plugin 'github_username/plugin_name'
+# set -g @plugin 'github_username/plugin_name#branch'
+# set -g @plugin 'git@github.com:user/plugin'
+# set -g @plugin 'git@bitbucket.com:user/plugin'
+
+# Initialize TMUX plugin manager (keep this line at the very bottom of tmux.conf)
+run '~/.tmux/plugins/tpm/tpm'
---
+++ home.nix
@@ -0,0 +1,260 @@
+{ pkgs, ... }:
+
+let
+  nixvim = import (builtins.fetchGit {
+    url = "https://github.com/nix-community/nixvim";
+  });
+in
+{
+  imports = [
+    nixvim.homeManagerModules.nixvim
+  ];
+  # Define the state version, which corresponds to the version of Home Manager
+  # you are using. This should be updated whenever you update Home Manager.
+  home.stateVersion = "24.05";
+
+  # Enable Home Manager to manage your home directory.
+  programs.home-manager.enable = true;
+
+  # Set up some basic settings for the home environment.
+  home.username = "boneypatel";
+  home.homeDirectory = "/Users/boneypatel";
+
+  # Define the Home Manager environment variables.
+  home.sessionVariables = {
+    EDITOR = "nvim";
+    VISUAL = "nvim";
+    LANG = "en_US.UTF-8";
+  };
+
+  # Define user-specific aliases.
+  home.shellAliases = {
+    lg = "lazygit";
+    v = "nvim";
+  };
+
+  # Enable custom fonts
+  fonts.fontconfig.enable = true;
+
+  # Specify the desired packages to install in the user environment.
+  home.packages = with pkgs; [
+    vim
+    git
+    curl
+    wget
+    lazygit
+    tmux
+    ripgrep
+    zsh-powerlevel10k
+  ];
+
+  # Enable and configure some basic programs.
+  programs.git = {
+    enable = true;
+    userName = "Boney Patel";
+    userEmail = "boneypatel37@yahoo.com";
+
+    extraConfig = {
+      init.defaultBranch = "main";
+    };
+  };
+
+  programs.ssh = {
+    enable = true;
+    forwardAgent = true;
+  };
+
+  programs.zsh = {
+    enable = true;
+    oh-my-zsh = {
+      theme = "powerlevel10k/powerlevel10k";
+      enable = true;
+      plugins = [ "git" "z" "sudo" "powerlevel10k"];
+    };
+  };
+
+  # Tmux configs
+  programs.tmux = {
+    enable = true;
+    clock24 = true;
+    tmuxinator.enable = true;
+
+    extraConfig = builtins.readFile ./extraConfig.tmux;
+  };
+
+  # Nixvim configs
+  programs.nixvim = {
+
+    # Keymaps
+    keymaps = 
+    [
+      {
+        action = ":Oil<CR>";
+        key = "<leader>o";
+        options = {
+          silent = true;
+        };
+      }
+      {
+        action = ":GitBlameOpenCommitURL<CR>";
+        key = "<leader>gb";
+        options = {
+          silent = true;
+        };
+      }
+      {
+        action = ":LazyGit<CR>";
+        key = "<leader>lg";
+        options = {
+          silent = true;
+        };
+      }
+      {
+        action = ":PreviewMarkdown<CR>";
+        key = "<leader>pm";
+        options = {
+          silent = true;
+        };
+      }
+      {
+        action = ":Toggle direction=float<CR>";
+        key = "<leader>f";
+        options = {
+          silent = true;
+        };
+      }
+      {
+        action = ":Toggle<CR>";
+        key = "<leader>b";
+        options = {
+          silent = true;
+        };
+      }
+
+    ];
+  
+    globals = {
+      mapleader = " ";
+      maplocalleader = " ";
+    };
+
+    enable = true;
+    defaultEditor = true;
+
+    viAlias = true;
+    vimAlias = true;
+
+    luaLoader.enable = true;
+    colorschemes.catppuccin.enable = true;
+    clipboard.register = "unnamedplus";
+
+    # Options
+    opts = {
+      number = true;
+      relativenumber = true;
+
+      shiftwidth = 4;
+
+    };
+    # Neovim plugins
+    plugins = {
+
+      # Nix
+      nix = {
+        enable = true;
+      };
+
+      # Best git extension
+      lazygit = {
+        enable = true; 
+      };
+
+      # Git blame for git history
+      gitblame = {
+        enable = true;
+      };
+
+      # Multiple cursors
+      multicursors = {
+        enable = true;
+      };
+
+      # File explorer buffer
+      oil = {
+        enable = true; 
+      };
+
+      # Navigate from tmux <--> neovim
+      tmux-navigator = {
+        enable = true;
+      };
+
+      # Tracks keybindings
+      which-key = {
+        enable = true;
+      };
+
+      # Floating terminal
+      toggleterm = {
+        enable = true;
+      };
+
+      # Fuzzy finding and searching
+      telescope = {
+        enable = true;
+      };
+
+      # Render markdown files while editing
+      markdown-preview = {
+        enable = true;
+      };
+
+      # Tabs for buffers
+      barbar = {
+        enable = true;
+        keymaps = {
+          previous.key = "<S-Tab>";
+          next.key = "<Tab>";
+          close.key = "<leader>x";
+        };
+      };
+
+      # Illuminate hover words
+      illuminate = {
+        enable = true;
+      };
+
+      # Rainbow delimiters
+      rainbow-delimiters = {
+        enable = true;
+      };
+
+      # Language server protocols
+      lsp = {
+        enable = true;
+	servers = {
+
+	  # Nix LSP
+	  nil-ls.enable = true;
+
+	  # Python
+	  pyright.enable = true;
+
+	  # Markdown
+	  marksman.enable = true;
+
+	  # Bash
+	  bashls.enable = true;
+
+	};
+      };
+    };
+
+    extraPlugins = with pkgs.vimPlugins;
+      [
+        nvim-web-devicons
+      ];
+  };
+
+}
+
