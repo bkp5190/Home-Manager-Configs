095e011634ecf021f9a747de31f7043f12b58129
bpatel347 <bpatel347@gatech.edu>
2025-12-20 23:36:09 -0500

    Additional cleanup

--- home.nix
+++ home.nix
@@ -103,23 +103,15 @@ in
   ];
 
   programs = {
-
-    java = {
-      enable = true;
-      package = pkgs.openjdk17;
-    };
     # Enable Home Manager to manage your home directory.
     home-manager.enable = true;
-
     # direnv
     direnv.enable = true;
-
     # Enable and configure some basic programs.
     git = {
       enable = true;
       userName = "bpatel347";
       userEmail = "bpatel347@gatech.edu";
-
       extraConfig = {
         init.defaultBranch = "main";
       };
@@ -209,7 +201,7 @@ in
       '';
     };
 
-    # Atuin
+    # Atuin terminal history scroller 
     atuin = {
       enable = true;
       enableZshIntegration = true;
@@ -221,94 +213,6 @@ in
       };
     };
 
-    # tmux = {
-    #   enable = true;
-    #   shell = "${pkgs.writeShellScriptBin "zsh-login" ''
-    #   exec ${pkgs.zsh}/bin/zsh -l
-    #   ''}/bin/zsh-login";
-    #   terminal = "tmux-256color";
-    #   historyLimit = 100000;
-    #   plugins = with pkgs;
-    #     [
-    #       tmux-nvim
-    #       tmuxPlugins.tmux-thumbs
-    #       tmuxPlugins.sensible
-    #       # must be before continuum edits right status bar
-    #       {
-    #         plugin = tmuxPlugins.catppuccin;
-    #         extraConfig = '' 
-    #           set -g @catppuccin_flavour 'frappe'
-    #           set -g @catppuccin_window_tabs_enabled on
-    #           set -g @catppuccin_date_time "%H:%M"
-    #         '';
-    #       }
-    #       {
-    #         plugin = tmuxPlugins.resurrect;
-    #         extraConfig = ''
-    #           set -g @resurrect-strategy-vim 'session'
-    #           set -g @resurrect-strategy-nvim 'session'
-    #           set -g @resurrect-capture-pane-contents 'on'
-    #         '';
-    #       }
-    #       {
-    #         plugin = tmuxPlugins.continuum;
-    #         extraConfig = ''
-    #           set -g @continuum-restore 'on'
-    #           set -g @continuum-boot 'on'
-    #           set -g @continuum-save-interval '10'
-    #         '';
-    #       }
-    #       tmuxPlugins.better-mouse-mode
-    #       tmuxPlugins.yank
-    #     ];
-    #   extraConfig = ''
-    #     set -g default-terminal "tmux-256color"
-    #     set -ag terminal-overrides ",xterm-256color:RGB"
-    #     set-option -g prefix M-space
-    #     unbind C-b
-    #     bind M-space send-prefix
-    #
-    #     set -g mouse on
-    #     set-option -g set-clipboard on
-    #
-    #     setw -g mode-keys vi
-    #
-    #     bind -T copy-mode-vi v send -X begin-selection
-    #     bind -T copy-mode-vi y send -X copy-selection-and-cancel
-    #     bind -T copy-mode-vi C-v send -X rectangle-toggle
-    #     bind Escape copy-mode
-    #
-    #     unbind '"'
-    #     unbind %
-    #     bind | split-window -h -c "#{pane_current_path}"
-    #     bind - split-window -v -c "#{pane_current_path}"
-    #
-    #     bind -n M-h select-pane -L
-    #     bind -n M-j select-pane -D
-    #     bind -n M-k select-pane -U
-    #     bind -n M-l select-pane -R
-    #
-    #     bind -n M-Left  previous-window
-    #     bind -n M-Right next-window
-    #
-    #     bind -n M-h resize-pane -L 5
-    #     bind -n M-l resize-pane -R 5
-    #     bind -n M-j resize-pane -D 2
-    #     bind -n M-k resize-pane -U 2
-    #     bind r source-file ~/.config/tmux/tmux.conf \; display-message "Config reloaded!"
-    #
-    #     unbind p
-    #     bind p paste-buffer
-    #
-    #     set-option -g status-position top
-    #
-    #     bind-key -T prefix C-l switch -t notes
-    #     bind-key -T prefix C-d switch -t dotfiles
-    #     bind-key -T prefix C-g split-window "$SHELL --login -i -c 'navi --print | head -c -1 | tmux load-buffer -b tmp - ; tmux paste-buffer -p -t {last} -b tmp -d'"
-    #
-    #     set -g @yank_selection 'clipboard'
-    #   '';
-    #   };
     # Starship command history
     starship = {
       enable = true;
